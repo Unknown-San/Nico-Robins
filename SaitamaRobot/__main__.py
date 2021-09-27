e3a4120c974337154ce6b207aa412a88f157aadc
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-09-27 06:57:12 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -72,11 +72,11 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
- Yahho Watashi Nobara Desu, I am Groups Management Bot !! I can Help You Manage Your Group for you!!, send /help for command list...[.](https://telegra.ph/file/1f054a1f6e857f6c4428d.jpg)
+Heyo Watashi Nico Desu, I am Groups Management Bot !! I can Help You Manage Your Group for you!!, send /help for command list...[.](https://telegra.ph/file/36f504f4c0c2f0f138806.jpg)
 """
 
 HELP_STRINGS = """
-Hey there, I'm Nobara Kugisaki!
+Hey there, I'm Nico Robins!
 To make me functional, make sure that i have enough rights in your group.
 
 Helpful commands:
@@ -84,16 +84,16 @@ Helpful commands:
 - /help: Sends this message; I'll tell you more about myself!
 - /donate: Gives you info on how to support me and my creator.
 
-Join My Support If You Have Any Queries: @NobaraSupport
+Join My Support If You Have Any Queries: @NicoRobin_Support
 
 All commands can be used with the following: / !
 List of all the Modules:
 """.format(
     dispatcher.bot.first_name,
-    "" if not ALLOW_EXCL else "📝All commands can either be used with / or !.",
+    "" if not ALLOW_EXCL else "👀 All commands can either be used with / or !.",
 )
 
-EREN_IMG = "https://telegra.ph/file/8ab770904946c033f114e.jpg"
+EREN_IMG = "https://telegra.ph/file/3e06334617f26c66c4f2b.jpg"
 
 DONATE_STRING = """Durov Is my Cousin By The Way.."""
 
@@ -225,7 +225,7 @@ def start(update: Update, context: CallbackContext):
                             ),
                             InlineKeyboardButton(
                                 text="🚦 ᴜᴘᴅᴀᴛᴇs 🚦",
-                                url="https://t.me/NobaraBotUpdates",
+                                url="https://t.me/NicoRobin_updates",
                             ),
                         ],
                         [
@@ -235,13 +235,13 @@ def start(update: Update, context: CallbackContext):
                             ),
                             InlineKeyboardButton(
                                 text="ɢʀᴏᴜᴘ",
-                                url="https://t.me/The_Phantom_Troupe/",
+                                url="https://t.me/Loli_League/",
                             ),
                         ],
                         [
                             InlineKeyboardButton(
                                 text="⚓️ ʜᴇʟᴘ ⚓️",
-                                url="https://t.me/Nobara_superbot?start=help",
+                                url="https://t.me/NicoRobin_robot?start=help",
                             ),
                         ],
                     ],
@@ -249,7 +249,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "A jujutsu Sorcerer Never Sleeps!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "A Light of the Revolution Never Sleeps!\n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime,
             ),
             parse_mode=ParseMode.HTML,
@@ -667,7 +667,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Eren Jaeger is deployed successfully!")
+        LOGGER.info("RANDI is deployed successfully!")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
