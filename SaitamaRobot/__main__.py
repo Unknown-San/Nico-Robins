4edd706b5b11ffe0d473fe9e0f3f6716a2558891
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-11-12 22:31:17 +0530

    __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -244,7 +244,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "  I won't sleep until satisfy you 🥵🥵 ! \n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "  https://telegra.ph/file/a6b3a74e487f439b921db.mp4 | I won't sleep until satisfy you 🥵🥵 ! \n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime,
             ),
             parse_mode=ParseMode.HTML,
