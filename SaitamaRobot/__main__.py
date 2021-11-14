883419ae30c17934101600b659c91dac04629762
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-14 19:03:31 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -24,6 +24,7 @@ from SaitamaRobot import (
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from SaitamaRobot.modules import ALL_MODULES
+import SaitamaRobot.modules.sql.users_sql as sql
 from SaitamaRobot.modules.helper_funcs.chat_status import is_user_admin
 from SaitamaRobot.modules.helper_funcs.misc import paginate_modules
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
