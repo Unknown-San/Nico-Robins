5e88f8cad8a6175b7bf728bb9dcf7a604c4a8780
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-11-15 16:33:45 +0530

    __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -206,7 +206,7 @@ def start(update: Update, context: CallbackContext):
                     update.effective_chat.id,
                     HELPABLE[mod].__help__,
                     InlineKeyboardMarkup(
-                        [[InlineKeyboardButton(text="[► Back ◄]", callback_data="https://telegram.dog/Flare_Robot")]]
+                        [[InlineKeyboardButton(text="[► Back ◄]", callback_data="help_back")]]
                     ),
                 )
 
