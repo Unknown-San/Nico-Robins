4cd30cae7fe231b3c1e9e187989d415e6f0192a4
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-11-18 20:57:56 +0530

    forceSubscrice_sql(1).py

--- SaitamaRobot/modules/sql/forceSubscrice_sql(1).py
+++ SaitamaRobot/modules/sql/forceSubscrice_sql(1).py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from GabiBraunRobot.modules.sql import BASE, SESSION
+from SaitamaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
