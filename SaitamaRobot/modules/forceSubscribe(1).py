a80453e5a82a3adf4da454547cf344ad9ef00d17
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-11-18 20:59:46 +0530

    forceSubscribe(1).py

--- SaitamaRobot/modules/forceSubscribe(1).py
+++ SaitamaRobot/modules/forceSubscribe(1).py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from GabiBraunRobo import DRAGONS as SUDO_USERS
-from GabiBraunRobot import pbot
-from GabiBraunRobot.modules.sql import forceSubscribe_sql as sql
+from SaitamaRobot import DRAGONS as SUDO_USERS
+from SaitamaRobot import pbot
+from SaitamaRobot.modules.sql import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
