6d6a1cae2f4abdba4c7226523b5524f32fcff4bf
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-08 10:34:20 +0530

    __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -93,7 +93,7 @@ if ENV:
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
     BOT_ID = os.environ.get ("BOT_ID", None)
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", Flare_Robot)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
