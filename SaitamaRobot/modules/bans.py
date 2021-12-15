32ecf6450424df6bb5911f4fe48d31f5aafdad03
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-15 20:05:21 +0530

    bans.py

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -106,7 +106,7 @@ def ban(update: Update, context: CallbackContext) -> str:
         log += "\n<b>Reason:</b> {}".format(reason)
 
     try:
-        chat.ban_member(user_id)
+        chat.kick_member(user_id)
 
         if silent:
             if message.reply_to_message:
